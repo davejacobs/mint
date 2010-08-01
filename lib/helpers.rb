a194e450e89a045014141522f3a5d9f2befb3a9e
David Jacobs <dbjacobs@gmail.com>
2010-07-31 21:11:11 -0400

    add helpers file - should've included it before

---
+++ lib/helpers.rb
@@ -0,0 +1,17 @@
+require 'pathname'
+
+module Mint
+  module Helpers    
+    # Returns the relative path to dir1 from dir2.
+    def self.normalize_path(dir1, dir2)
+      path = case dir1
+        when String
+          Pathname.new dir1
+        when Pathname
+          dir1
+        end
+
+      path.expand_path.relative_path_from(dir2)
+    end
+  end
+end
