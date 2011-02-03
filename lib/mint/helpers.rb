cb1f114c0207e6fab85a942a2443b440b6b52732
David Jacobs <david@allthingsprogress.com>
2011-02-02 23:02:22 -0500

    add more helpers - slugize, symbolize, pathize, then rework normalize_path

--- lib/mint/helpers.rb
+++ lib/mint/helpers.rb
@@ -4,16 +4,19 @@ require 'yaml'
 
 module Mint
   module Helpers    
+    def self.pathize(str_or_path)
+      case str_or_path
+      when String
+        Pathname.new str_or_path
+      when Pathname
+        str_or_path
+      end.expand_path
+    end
+
     # Returns the relative path to dir1 from dir2.
     def self.normalize_path(dir1, dir2)
-      path = case dir1
-        when String
-          Pathname.new dir1
-        when Pathname
-          dir1
-        end
-
-      path.expand_path.relative_path_from(dir2.expand_path)
+      path1, path2 = [dir1, dir2].map {|d| pathize d }
+      path1.relative_path_from path2
     end
 
     def self.ensure_directory(dir)
@@ -26,6 +29,19 @@ module Mint
       File.open file, 'w' do |f|
         YAML.dump curr_opts.merge(new_opts), f
       end
+    end
+
+    def self.slugize(obj)
+      obj.to_s.downcase.
+        gsub(/&/, 'and').
+        gsub(/\s+/, '-').
+        gsub(/-+/, '-').
+        gsub(/[^a-z0-9-]/, '').
+        to_sym
+    end
+
+    def self.symbolize(obj)
+      obj.slugize.gsub(/-/, '_')
     end
   end
 end
