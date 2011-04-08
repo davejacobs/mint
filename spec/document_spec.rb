e46fbc35ab098630ce346b7e5e46a7b39426293e
David Jacobs <david@allthingsprogress.com>
2011-04-08 16:13:00 -0400

    revise and beef up tests, though they won't pass because of a proposed interface change that came up during the change

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -7,28 +7,28 @@ module Mint
       Dir.chdir 'tmp'
     end
 
-    before(:each) do 
-      @file = 'content.md'
+    before do 
+      @content_file = 'content.md'
       @layout_file = 'local.haml'
       @style_file = 'local.css'
 
       @content = <<-HERE.gsub(/^\s*/, '')
-          Header
-          ------
+        Header
+        ------
 
-          This is just a test.
+        This is just a test.
       HERE
 
       @layout = <<-HERE.gsub(/^\s*/, '')
-          !!!
-          %html
-            %head
-            %body= content
+        !!!
+        %html
+          %head
+          %body= content
       HERE
 
       @style = 'body { font-size: 16px }'
 
-      File.open @file, 'w' do |f|
+      File.open @content_file, 'w' do |f|
         f << @content
       end
 
@@ -41,106 +41,118 @@ module Mint
       end
     end
 
-    after(:all) do
+    after do
       File.delete @content
       File.delete @layout_file
       File.delete @style_file
-      Dir.chdir @old_dir
     end
+
+    after(:all) { Dir.chdir @old_dir }
 
     shared_examples_for "all documents" do
       it "loads content as its source" do
-        @document.source.to_s.should == 'content.md'
+        document.source.to_s.should == 'content.md'
       end
 
       it "guesses its name from the source" do
-        @document.name.to_s.should =~ /content/
+        document.name.to_s.should =~ /content/
       end
 
       it "renders its content" do
-        @document.content.should =~ /<p>This is just a test.<\/p>/
+        document.content.should =~ /<p>This is just a test.<\/p>/
       end
 
       it "renders its layout, injecting content inside" do
-        @document.render.should =~ /.*<html>.*<\/html>.*/m
-        @document.render.should =~ /<p>This is just a test.<\/p>/
+        document.render.should =~ 
+          /.*<html>.*<p>This is just a test.<\/p>.*<\/html>.*/m
       end
 
-      it "writes its rendered layout + content to its destination directory" do
-        @document.mint
-        file = Pathname.getwd + (@document.destination || '') + @document.name
-        file.exist?.should == true
+      # This test makes me think I want to add document.destination_file and
+      # change document.destination to document.destination_directory.
+      it "creates a named output file in its specified destination directory" do
+        document.mint
+        document.destination_file.should exist
+        # file = Pathname.getwd + (document.destination || '') + document.name
+        # file.should exist
+      end
+
+      it "writes its rendered layout and content to that output file" do
+        document.mint
+        content = File.read document.destination_file
+        content.should =~ /.*<html>.*<p>This is just a test.<\/p>.*<\/html>.*/m
       end
     end
 
     shared_examples_for "documents with a static stylesheet" do
-      it "does not render its style" do
-        @document.style.needs_rendering?.should == false
+      it "knows not to render its style" do
+        document.style.should_not need_rendering
       end
+      
+      it "does not render or write its style"
     end
 
     shared_examples_for "documents with a dynamic stylesheet" do
-      it "renders its style" do
-        @document.style.needs_rendering?.should == true
+      it "knows to render its style" do
+        document.style.should_not need_rendering
       end
+
+      it "writes its rendered style into its style_destination"
     end
 
     shared_examples_for "documents with the default template" do
       it "chooses the default layout" do
-        @document.layout.should be_in_directory('default')
+        document.layout.should be_in_directory('default')
       end
 
       it "chooses the default style" do
-        @document.style.should be_in_directory('default')
+        document.style.should be_in_directory('default')
       end
     end
 
     shared_examples_for "documents with the pro template" do
       it "chooses the pro layout" do
-        @document.layout.should be_in_directory('pro')
+        document.layout.should be_in_directory('pro')
       end
 
       it "chooses the pro style" do
-        @document.style.should be_in_directory('pro')
+        document.style.should be_in_directory('pro')
       end
     end
 
     shared_examples_for "documents with the local template" do
       it "chooses the local layout" do
-        @document.layout.name.to_s.should =~ /local/
-        @document.layout.source.to_s.should == 'local.haml'
+        document.layout.name.to_s.should =~ /local/
+        document.layout.source.to_s.should == 'local.haml'
       end
 
       it "chooses the local style" do
-        @document.style.name.to_s.should =~ /local/
-        @document.style.source.to_s.should == 'local.css'
+        document.style.name.to_s.should =~ /local/
+        document.style.source.to_s.should == 'local.css'
       end
     end
 
     shared_examples_for "documents without explicit directories" do
       it "does not have a nested destination directory" do
-        @document.destination.should be_nil
+        document.destination.should be_nil
       end
 
       it "sets the style's current directory as its style destination" do
-        @document.style_destination.should be_nil
+        document.style_destination.should be_nil
       end
     end
 
     shared_examples_for "documents with explicit directories" do
       it "has a nested destination directory" do
-        @document.destination.to_s.should == 'destination'
+        document.destination.to_s.should == 'destination'
       end
 
       it "sets the destination directory as its style destination" do
-        @document.style_destination.to_s.should == 'styles'
+        document.style_destination.to_s.should == 'styles'
       end
     end
 
     context "when it's created without explicit options" do
-      before(:each) do
-        @document = Document.new @file
-      end
+      let(:document) { Document.new @content_file }
 
       it_behaves_like "all documents"
       it_behaves_like "documents with the default template"
@@ -149,9 +161,8 @@ module Mint
     end
 
     context "when it's created inline with named layouts and styles" do
-      before(:each) do
-        @document = Document.new @file, :layout => 'pro', :style => 'pro'
-      end
+      let(:document) { Document.new @content_file, 
+                       :layout => 'pro', :style => 'pro' }
 
       it_behaves_like "all documents"
       it_behaves_like "documents with the pro template"
@@ -160,10 +171,8 @@ module Mint
     end
 
     context "when it's created inline with local layouts and styles" do
-      before(:each) do
-        @document = Document.new @file, :layout => 'local.haml', 
-          :style => 'local.css'
-      end
+      let(:document) { Document.new @content_file, 
+                       :layout => 'local.haml', :style => 'local.css' }
 
       it_behaves_like "all documents"
       it_behaves_like "documents with the local template"
@@ -172,9 +181,7 @@ module Mint
     end
 
     context "when it's created with a template" do
-      before(:each) do
-        @document = Document.new @file, :template => 'pro'
-      end
+      let(:document) { Document.new @content_file, :template => 'pro' }
 
       it_behaves_like "all documents"
       it_behaves_like "documents with the pro template"
@@ -183,10 +190,9 @@ module Mint
     end
 
     context "when it's created with a non-default destinations" do
-      before(:each) do
-        @document = Document.new @file, :root => 'root',
-          :destination => 'destination', :style_destination => 'styles'
-      end
+      let(:document) { Document.new @content_file, 
+                       :root => 'root', :destination => 'destination',
+                       :style_destination => 'styles' }
 
       it_behaves_like "all documents"
       it_behaves_like "documents with the default template"
@@ -195,11 +201,10 @@ module Mint
     end
 
     context "when it's created with a block" do
-      before(:each) do
-        @document = Document.new @file do |document|
+      let(:document) do
+        Document.new @file do |document|
           document.destination = 'destination'
           document.style_destination = 'styles'
-
           document.layout = 'pro'
           document.style = 'pro'
         end
