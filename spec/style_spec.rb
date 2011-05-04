41c72baee5d3267d52e5e4094704ea77f6599b4b
David Jacobs <david@allthingsprogress.com>
2011-05-04 14:53:10 -0400

    update style spec style

--- spec/style_spec.rb
+++ spec/style_spec.rb
@@ -4,62 +4,45 @@ module Mint
   describe Style do
     context "when it's created from a static file" do
       let(:style) { Style.new @static_style_file }
+      subject { style }
 
-      it "#rendered?" do
-        style.should_not be_rendered
-      end
+      its(:destination) { should be_nil }
+      its(:destination_file) { should == '/tmp/mint-test/static.css' }
 
-      it "#render" do
+      it { should_not be_rendered }
+      it "'renders' itself verbatim" do
         style.render.should == File.read(@static_style_file)
-      end
-
-      it "#destination" do
-        style.destination.should be_nil
-      end
-
-      it "#destination_file" do
-        style.destination_file.should == '/tmp/mint-test/static.css'
       end
     end
 
     context "when it's created from a dynamic file" do
       let(:style) { Style.new @dynamic_style_file }
+      subject { style }
 
-      it "#rendered?" do
-        style.should be_rendered
-      end
+      its(:destination) { should be_nil }
+      its(:destination_file) { should == '/tmp/mint-test/dynamic.css' }
 
-      it "#render" do
+      it { should be_rendered }
+      it "renders itself from a templating language to Html" do
         style.render.gsub("\n", " ").should == 
           File.read(@static_style_file).gsub("\n", " ")
-      end
-
-      it "#destination" do
-        style.destination.should be_nil
-      end
-
-      it "#destination_file" do
-        style.destination_file.should == '/tmp/mint-test/dynamic.css'
       end
     end
 
     context "when it's created with a specified destination" do
       let(:style) { Style.new @static_style_file,
                     :destination => 'destination' }
+      subject { style }
 
-      it "#destination" do
-        style.destination.should == 'destination'
-      end
-
-      it "#destination_file" do
-        style.destination_file.should == 
-          '/tmp/mint-test/destination/static.css'
+      its(:destination) { should == 'destination' }
+      its(:destination_file) do
+        should == '/tmp/mint-test/destination/static.css'
       end
     end
 
     # TODO: Create local-scope templates directory that I can test this with,
     # and use it to beef up other specs (like document_spec and mint_spec)
-    context "when it's created from a static template file" do
+    # context "when it's created from a static template file" do
     #  let(:style) { Style.new(Mint.lookup_template(:static_test, :style)) }
     #  it "#destination" do
     #    style.destination.should be_nil
@@ -69,17 +52,15 @@ module Mint
     #    style.destination_file.should == 
     #      Mint.path_for_scope(:local) + '/templates/static_test/style.css'
     #  end
-    end
+    # end
 
     context "when it's created from a dynamic template file" do
       let(:style) { Style.new(Mint.lookup_template(:default, :style)) }
-      it "#destination" do
-        style.destination.should == 'css'
-      end
+      subject { style }
 
-      it "#destination_file" do
-        style.destination_file.should == 
-          "#{Mint.root}/templates/default/css/style.css"
+      its(:destination) { should == 'css' }
+      its(:destination_file) do
+        should == "#{Mint.root}/templates/default/css/style.css"
       end
     end
   end
