478a75a28dbdcfc4c4567ac9dc68a1e6cbc66755
David Jacobs <david@wit.io>
2011-08-10 20:55:41 -0700

    add yardoc params to layout

--- lib/mint/layout.rb
+++ lib/mint/layout.rb
@@ -4,6 +4,9 @@ module Mint
   class Layout < Resource
     # Creates a new Layout object using a mandatory source file
     # and optional configuration options.
+    #
+    # @param [String] source the absolute or relative file path
+    # @param [Hash, #[]] opts layout options
     def initialize(source, opts=Mint.default_options)
       super(source, opts)
       self.type = :layout
