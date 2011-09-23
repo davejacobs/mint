50ec2b1476b4ee85aed49b9f25afeb66d7c6f1a5
David Jacobs <david@wit.io>
2011-09-22 22:27:49 -0700

    begin to craft plugin system

--- lib/mint/plugin.rb
+++ lib/mint/plugin.rb
@@ -1,27 +1,82 @@
 require 'mint/document'
+require 'set'
 
 module Mint
-  def self.register_plugin(plugin)
-    @plugins << plugin
+  def self.plugins
+    (@@plugins || Set.new).to_a
   end
 
-  def self.process
-    
+  def self.register_plugin!(plugin)
+    @@plugins ||= Set.new
+    @@plugins << plugin
   end
-  
-  def self.process_with(plugin)
+
+  def self.clear_plugins!
+    @@plugins.clear
+  end
+
+  def self.before_render(plain_text)
+    plugins.reduce(plain_text) do |intermediate, plugin|
+      plugin.before_render(intermediate)
+    end
+  end
+
+  def self.after_render(html_text)
+    plugins.reduce(html_text) do |intermediate, plugin|
+      plugin.after_render(intermediate)
+    end
+  end
+
+  def self.after_publish(file)
+    plugins.each do |plugin|
+      plugin.after_publish(file)
+    end
   end
 
   class Plugin
-    def self.inherited(subclass)
-      Mint.register_plugin(subclass)
+    def self.inherited(plugin)
+      Mint.register_plugin! plugin
     end
 
     def commandline_options
     end
 
-    def before_render; end
-    def after_render; end
-    def after_mint; end
+    # Supports:
+    # - Change raw text
+    #
+    # Use cases:
+    # - Add footnote syntax on top of Markdown
+    # - Perform text analysis for use in later callbacks (?)
+    def self.before_render(text_document); end
+
+    # Supports:
+    # - Change preview HTML
+    #
+    # Use cases:
+    # - Transform elements based on position or other HTML attributes
+    #   For example: Add a class to the first paragraph of a document if it is
+    #   italicized
+    #
+    # Questions:
+    # - Could I allow jQuery use here?
+    def self.after_render(html_document); end
+
+    # Supports:
+    # - Change file, filesystem once written
+    # - Automatic cleanup of intermediate files, including all edge cases
+    #   currently covered by transformation library. (For example, if I generated
+    #   a CSS file but am ultimately generating a PDF, I would want to have 
+    #   an automatic way to delete that CSS file.)
+    #
+    # Use cases:
+    # - Zip set of documents into ePub and create manifest 
+    # - Change file extension
+    # - Generate PDF from emitted HTML and get rid of intermediate files
+    # - Generate .doc (or any other OO UNO format) and get rid of intermediate files
+    #
+    # NOTE: Unlike the other two callbacks, this doesn't use the result of 
+    #   the callback expression for anything. This callback is purely for 
+    #   side effects like rearranging the file system.
+    def self.after_publish(file); end
   end
 end
---
+++ spec/plugin_spec.rb
@@ -0,0 +1,141 @@
+require 'spec_helper'
+
+describe Mint do
+  describe "#plugins"
+
+  describe "#register_plugin!" do
+    before { @plugin = Class.new }
+    after { Mint.clear_plugins! }
+
+    it "registers a plugin once" do
+      Mint.register_plugin! @plugin
+      Mint.plugins.should == [@plugin]
+    end
+
+    it "does not register a plugin more than once" do
+      Mint.register_plugin! @plugin
+      lambda { Mint.register_plugin! @plugin }.should_not change { Mint.plugins }
+      Mint.plugins.should == [@plugin]
+    end
+  end
+
+  describe "#clear_plugins!" do
+    before { @plugin = Class.new }
+    after { Mint.clear_plugins! }
+
+    it "does nothing if no plugins are registered" do
+      lambda { Mint.clear_plugins! }.should_not raise_error
+    end
+
+    it "removes all registered plugins" do
+      Mint.register_plugin! @plugin
+      lambda { Mint.clear_plugins! }.should change { Mint.plugins.length }.by(-1)
+    end
+  end
+
+  [:before_render, :after_render].each do |callback|
+    describe "##{callback}" do
+      before do
+        @base_plugin   = Class.new(Mint::Plugin)
+        @first_plugin  = Class.new(@base_plugin)
+        @second_plugin = Class.new(@base_plugin)
+
+        @base_plugin.should_receive(callback).
+          ordered.and_return('transformed by base')
+        @first_plugin.should_receive(callback).
+          ordered.and_return('transformed by first')
+        @second_plugin.should_receive(callback).
+          ordered.and_return('transformed by second')
+      end
+
+      after { Mint.clear_plugins! }
+
+      it "reduces ##{callback} across all registered plugins in order" do
+        Mint.send(callback, 'text').should == 'transformed by second'
+      end
+    end
+  end
+
+  describe "#after_publish" do
+    before do
+      @base_plugin   = Class.new(Mint::Plugin)
+      @first_plugin  = Class.new(@base_plugin)
+      @second_plugin = Class.new(@base_plugin)
+
+      @base_plugin.should_receive(:after_publish).ordered.and_return(nil)
+      @first_plugin.should_receive(:after_publish).ordered.and_return(nil)
+      @second_plugin.should_receive(:after_publish).ordered.and_return(nil)
+    end
+
+    after { Mint.clear_plugins! }
+
+    it "calls each registered plugin in order, passing it a document" do
+      # Mint.after_publish(document)
+    end
+  end
+
+  describe Mint::Plugin do
+    before do
+      @base_plugin   = Class.new(Mint::Plugin)
+      @first_plugin  = Class.new(@base_plugin)
+      @second_plugin = Class.new(@base_plugin)
+    end
+
+    after { Mint.clear_plugins! }
+
+    describe "#inherited" do
+      it "registers the subclass with Mint as a plugin" do
+        lambda do
+          @plugin = Class.new(Mint::Plugin)
+        end.should change { Mint.plugins.length }.by(1)
+      end
+
+      it "preserves the order of subclassing" do
+        Mint.plugins.should == [@base_plugin, @first_plugin, @second_plugin]
+      end
+
+      it "does not change the order of a plugin when it is monkey-patched" do
+        lambda do
+          @base_plugin.instance_eval { def monkey_patch; end }
+        end.should_not change { Mint.plugins }
+      end
+    end
+
+    describe "#commandline_options"
+
+    context "plugin callbacks" do
+      before do
+        @base_plugin   = Class.new(Mint::Plugin)
+        @first_plugin  = Class.new(@base_plugin)
+        @second_plugin = Class.new(@base_plugin)
+
+        @base_plugin.should_receive(:before_render).
+          ordered.and_return('transformed by base')
+        @first_plugin.should_receive(:before_render).
+          ordered.and_return('transformed by first')
+        @second_plugin.should_receive(:before_render).
+          ordered.and_return('transformed by second')
+      end
+
+      describe "#before_render"
+      describe "#after_render"
+      describe "#after_mint" do
+        # let(:document) { Document.new 'content.md' }
+        #
+        # it "allows changes to the document extension" do
+          # Mint.should_call(:process_with).once.with(plugin)
+          # Mint.publish!(document)
+        # end
+        # it "allows changes to the document type"
+        # it "allows changes to the document structure"
+        # it "allows changes to the document content"
+
+        it "allows changes to final published document file"
+        it "allows changes to final published style file"
+        it "allows changes to final published document directory"
+        it "allows changes to final published style directory"
+        it "allows changes to final published style directory"
+      end
+    end
+  end
+end
