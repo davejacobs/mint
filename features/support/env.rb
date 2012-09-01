c067d1647bdc53de69231d01b9908be4b5f8a248
David Jacobs <david@wit.io>
2012-09-01 14:26:32 -0700

    clean up cucumber support file

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,32 +1,15 @@
 require "aruba/cucumber"
 
-$LOAD_PATH.unshift File.expand_path("../../../lib", __FILE__)
-
-module ArubaOverrides
-  def detect_ruby(cmd)
-    if cmd =~ /^mint/
-      "ruby -I ../../lib -S ../../bin/#{cmd}"
-    else
-      super(cmd)
-    end
-  end
-end
-
-World(ArubaOverrides)
-
 Before do
-  ENV["MINT_NO_PIPE"] = "true"
-  @aruba_timeout_seconds = 5
-  @old_path = ENV["PATH"]
-  @bin_path = File.expand_path("../../../bin", __FILE__)
-
-  unless @old_path.include? @bin_path
-    system "export PATH=#{@bin_path}:$PATH"
-  end
+  @aruba_timeout_seconds            = 5
+  @original_path, ENV["PATH"]       = ENV["PATH"], "../../bin:#{ENV['PATH']}"
+  @original_rubylib, ENV["RUBYLIB"] = ENV["RUBYLIB"], "../../lib"
+  ENV["MINT_NO_PIPE"]               = "true"
 end
 
 After do
-  FileUtils.rm_rf "tmp/aruba/.mint"
-  ENV["PATH"] = @old_path
+  ENV["PATH"]         = @original_path
+  ENV["RUBYLIB"]      = @original_rubylib
   ENV["MINT_NO_PIPE"] = nil
+  FileUtils.rm_rf "tmp/aruba"
 end
